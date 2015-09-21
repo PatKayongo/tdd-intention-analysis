166cc7fee80cc6834cb8aafe75d995e34f672786
Patrick Kayongo <patrick.kayongo@standardbank.co.za>
2015-09-21 08:07:00 +0200

    Perform summary statistics on numeric data only.

--- 3_summary_statistics.R
+++ 3_summary_statistics.R
@@ -1,13 +1,17 @@
 
-dir.create("Summary")
-
-means <- sapply(results, mean, na.rm=FALSE)
-write.csv(means, file="Summary/means.csv")
-
-standardDeviations <- sapply(results, sd, na.rm=FALSE)
-write.csv(standardDeviations, file="Summary/standardDeviations.csv")
+dir.create("Summary", showWarnings = FALSE)
 
 numericVariableNames <- c("YearsExperience", "Attitude","AttitudeTime","AttitudeMaintainability","AttitudeEfficiency","AttitudeChangeability","SubjectiveNormTime","SubjectiveNormMaintainability","SubjectiveNormQuality","SubjectiveNormChangeability","TeamTDD","UnitTestDifficulty","TDDDifficulty","TDDExperience","Intention","SubjectiveNorm","AttitudeQuality")
 numericData <- results[numericVariableNames]
+
+means <- sapply(numericData, mean, na.rm=FALSE)
+write.csv(means, file="Summary/means.csv")
+rm(means)
+
+standardDeviations <- sapply(numericData, sd, na.rm=FALSE)
+write.csv(standardDeviations, file="Summary/standardDeviations.csv")
+rm(standardDeviations)
+
 correlations <- cor(numericData)
 write.csv(correlations, file="Summary/correlations.csv")
+rm(correlations)
